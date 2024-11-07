ac8731bb2d55d2da3a689bcce491045c47add3e2
Geon05 <zqrc05@gmail.com>
2024-11-07 12:32:15 +0900

    20241107

--- django/mysite/mysite/urls.py
+++ django/mysite/mysite/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from .views import main
 
 urlpatterns = [
-    path("", include("chat.urls")),
+    path("htmltest/", include("htmltest.urls")),
+    path("chat/", include("chat.urls")),
     path('admin/', admin.site.urls),
 ]
