0e0ab9d49fe0af258e9c2d8bdadc3e3f6d705b7d
Geon05 <zqrc05@gmail.com>
2024-11-08 10:09:59 +0900

    20241108

---
+++ django/mysite/htmltest/static/htmltest/js/talk.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("JavaScript 파일이 성공적으로 로드되었습니다!");
+
+    // 😎수정부분
+    result = request.sendrequest
+    ({
+        url: "/myapp/hello/",
+        body: param
+    })
+    
+    // 추가적인 JavaScript 코드 작성
+});
--- django/mysite/htmltest/views.py
+++ django/mysite/htmltest/views.py
@@ -15,3 +15,19 @@ def testpage(request):
         "results": results,
     }
     return render(request, "htmltest/testpage.html", context)
+
+from django.shortcuts import render
+
+# 😎수정부분
+@app.route("/myapp/hello")
+def hello0(str: ):
+    data = python function 호출
+    
+    return data
+    
+    
+def testpage(request):
+    context = {
+        'star_range': range(1, 16)  # 1부터 15까지의 범위를 템플릿에 전달
+    }
+    return render(request, "htmltest/testpage.html", context)
