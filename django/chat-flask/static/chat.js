c99773cd032d6f448ec06b5a312120b5c81f7e24
Geon05 <zqrc05@gmail.com>
2024-11-11 20:16:41 +0900

    20241111

--- django/chat-flask/static/chat.js
+++ django/chat-flask/static/chat.js
@@ -1,4 +1,27 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const chatContainer = document.getElementById('chat-container');
-    chatContainer.innerHTML = "<p>This is a chat area!</p>";
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("JavaScript 파일이 성공적으로 로드되었습니다!");
 });
+
+const app = Vue.createApp({
+    data() {
+        return {
+            inputQuestion: '',
+            inputSubject: '',
+            chatHistory: [],
+            starRange: Array.from({ length: 15 }, (_, i) => i + 1),
+            subjectResponse: '',
+        };
+    },
+    methods: {
+        async subject_chk() {
+            fetch('/send_subject', { method: 'POST' })
+                .then(response => response.json())
+                .then((data) => {
+                    
+                })
+        }
+    },
+    computed: {
+
+    }
+})
