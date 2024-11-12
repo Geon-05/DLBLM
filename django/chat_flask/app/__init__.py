9c73631c84e189089418677d09008f804c544310
Geon05 <zqrc05@gmail.com>
2024-11-12 11:23:46 +0900

    20241112

--- django/chat_flask/app/__init__.py
+++ django/chat_flask/app/__init__.py
@@ -2,7 +2,9 @@ import os
 from flask import Flask
 
 def create_app(config_class='config.Config'):
-    app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), '../'))
+    app = Flask(__name__,
+                template_folder=os.path.join(os.path.dirname(__file__), '../templates'),
+                static_folder=os.path.join(os.path.dirname(__file__), '../static'))
     app.config.from_object(config_class)
 
     from .routes import main
--- django/chat_flask/app/routes.py
+++ django/chat_flask/app/routes.py
@@ -1,7 +1,33 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, send_from_directory, current_app, jsonify, request
+from app.emotion import emotion_chk
+
 
 main = Blueprint('main', __name__)
 
+# ico위치 명시
+@main.route('/favicon.ico')
+def favicon():
+    return send_from_directory(current_app.static_folder, 'favicon.ico')
+
+# index 앱으로 이동
 @main.route('/')
 def index():
-    return render_template('templates/chat.html')
+    return render_template('index.html')
+
+# chat봇 앱으로 이동
+@main.route('/chat')
+def chat():
+    return render_template('chat.html')
+
+# emotion 앱으로 이동
+@main.route('/emotion')
+def emotion():
+    return render_template('emotion.html')
+
+
+@main.route('/get_sentence', methods=['POST'])
+def perform_task():
+    data = request.get_json()
+    sentence = data.get('sentence', '')
+    emotion_result = emotion_chk(sentence)
+    return jsonify(emotion_result=emotion_result)
